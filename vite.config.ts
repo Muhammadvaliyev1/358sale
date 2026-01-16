d5277506afa26172416dacfbba0c009a316c383e
Muhammadvaliyev1 <127742778+Muhammadvaliyev1@users.noreply.github.com>
2026-01-16 23:32:21 +0500

    fixed host

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
+  preview: {
+    host: true, // слушать на 0.0.0.0
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['three58sale.onrender.com'] // разрешаем хост Render
   }
 });
